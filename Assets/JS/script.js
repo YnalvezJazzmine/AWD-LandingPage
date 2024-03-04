1e9d7a58250a0d8e6feca20c5dae095b1c5e446a
Jazzmine Llana Ynalvez <157238959+YnalvezJazzmine@users.noreply.github.com>
2024-03-04 22:58:46 +0800

    Add files via upload

---
+++ Assets/JS/script.js
@@ -0,0 +1,24 @@
+
+const projects = [
+    { name: "Jumping Letters" },
+    { name: "Notification Bell" },
+    { name: "Scroll Indicator" }
+];
+
+{
+    document.body.style.backgroundImage = "url('./assets/images/bg.jpg')";
+    }
+
+function populateProjects() {
+    const projectsList = document.getElementById('projects');
+    projects.forEach((project, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = project.name;
+        listItem.classList.add(`project${index + 1}`); 
+        projectsList.appendChild(listItem);
+    });
+}
+
+window.onload = populateProjects;
+
+
